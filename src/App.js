5f195e5fb0966dc3d8305aefd8275252915db100
Peter Wooden <peter.r.wooden@gmail.com>
2021-02-03 23:10:20 +1100

    install tailwind, remove tests and old css, first version of designer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { SketchPicker } from 'react-color'
+
+const FONT_WEIGHTS = ['normal', 'bold', 'lighter', 'bolder', 100, 200, 300, 400, 500, 600, 700, 800,900];
 
 function App() {
+  const [text, setText] = useState('Modern Logo');
+  const [color, setColor] = useState('#000');
+  const [fontWeight, setFontWeight] = useState('bolder');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="">
+        <input 
+          type="text" 
+          value={text} 
+          onChange={(e) => setText(e.target.value)}
+          style={{
+            color,
+            lineHeight: 'unset',
+            fontWeight
+          }}
+          className="shadow-lg p-5 text-3xl rounded-2xl text-center outline-none"
+        />
+
+      </div>
+
+      <select value={fontWeight} onChange={e => setFontWeight(e.target.value)}>
+        {FONT_WEIGHTS.map(weight => <option value={weight}>{weight}</option>)}
+      </select>
+
+      <SketchPicker onChange={color => setColor(color.hex)} color={color}/>
     </div>
   );
 }
